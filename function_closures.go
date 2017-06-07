fdb1dfa045e7bb9f76f932cfd78e0d4276114e75
yenkuanlee <yenkuanlee@gmail.com>
2017-06-07 16:07:20 +0800

    first commit

--- function_closures.go
+++ function_closures.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func adder() func(int) int {
 	sum := 0
+	// 如果有人宣告 a := adder(), a 是一個 function (因為 adder 的回傳型態為 func)
+	// a 裡面的 sum 初始值為 0, 之後就會一直變動, 像是 Java 裡面的 static
 	return func(x int) int {
 		sum += x
 		return sum
@@ -11,11 +13,11 @@ func adder() func(int) int {
 }
 
 func main() {
-	// 中文能打嗎?
 	pos, neg := adder(), adder()
-	for i := 0; i < 5; i++ {
+	// pos 中的 sum 初始值為 0
+	for i := 0; i < 10; i++ {
 		fmt.Println(
-			pos(i),
+			pos(i), // 每次呼叫 pos, 他的 sum 就會累計下去
 			neg(-2*i),
 		)
 	}
