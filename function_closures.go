3c655fad48e4eaba4a6037e411fdd25496ad6bad
yenkuanlee <yenkuanlee@gmail.com>
2017-06-07 15:58:47 +0800

    first commit

--- function_closures.go
+++ function_closures.go
@@ -11,8 +11,9 @@ func adder() func(int) int {
 }
 
 func main() {
+	// 中文能打嗎?
 	pos, neg := adder(), adder()
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 5; i++ {
 		fmt.Println(
 			pos(i),
 			neg(-2*i),
