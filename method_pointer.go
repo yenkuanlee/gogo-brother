3c6b8ba29ecb2ff4758e3c53ffec49c485919f08
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2017-06-08 14:16:53 +0800

    first commit

--- method_pointer.go
+++ method_pointer.go
@@ -1,3 +1,4 @@
+// Kevin Yen-Kuan Lee
 package main
 
 import (
@@ -9,10 +10,13 @@ type Vertex struct {
 	X, Y float64
 }
 
+// Abs 是 v 的屬性, 不會改變 v 本身
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// Scale 是 v 的屬性, 且 v 是 *Vertex, 故 Scale 可以改變 v 本身
+// 目前理解是這樣
 func (v *Vertex) Scale(f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
